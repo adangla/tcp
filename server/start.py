8a2e0d8c5feb5743fb0d522231ffe575ffe0d5a3
Aliona Dangla <aliona.dangla@outlook.fr>
2019-07-23 16:12:28 +0200

    Add Timeout in communication

--- server/start.py
+++ server/start.py
@@ -29,18 +29,24 @@ def start():
                 print(colors.OKBLUE + '...\tMESSAGE RECEIVED' + colors.ENDC)
                 nb_msg = 0
                 while True:
+                    data = None
                     # TODO: Check ack/seq and flags and reply a ACK
-                    r = sniff(filter='dst port 5555', count=1, iface="lo")
-                    if r and Raw in r[0] and r[0][TCP].flags == constant.PSH | constant.ACK:
+                    # TODO: Create conf file with timeout option
+                    data = sniff(filter='dst port 5555', count=1, iface="lo", timeout=10)
+                    if len(data) <= 0:
+                        # TODO: Handle timeout
+                        print('Timeout')
+                        break
+                    if data and Raw in data[0] and data[0][TCP].flags == constant.PSH | constant.ACK:
                         nb_msg += 1
-                        print(colors.BOLD + colors.WARNING + '[' + str(nb_msg) + ']: ' + r[0][Raw].load + colors.ENDC)
+                        print(colors.BOLD + colors.WARNING + '[' + str(nb_msg) + ']: ' + data[0][Raw].load + colors.ENDC)
                        
                         # Send ACK
                         ackdata = IP()/TCP()
-                        ackdata[TCP].sport    = r[0][TCP].dport
-                        ackdata[TCP].dport    = r[0][TCP].sport
-                        ackdata[TCP].seq      = r[0][TCP].ack
-                        ackdata[TCP].ack      = r[0][TCP].seq + len(r[0][Raw].load)
+                        ackdata[TCP].sport    = data[0][TCP].dport
+                        ackdata[TCP].dport    = data[0][TCP].sport
+                        ackdata[TCP].seq      = data[0][TCP].ack
+                        ackdata[TCP].ack      = data[0][TCP].seq + len(data[0][Raw].load)
                         ackdata[TCP].flags    = 'A'
 
                         send(ackdata, iface='lo')
