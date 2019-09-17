811731c3f12c7fbef0289998ee9db69f39839451
Aliona Dangla <aliona.dangla@outlook.fr>
2019-09-18 01:45:20 +0200

    Refactor client code, create class

--- client/start.py
+++ client/start.py
@@ -1,58 +1,68 @@
 from scapy.all import *
-from shared import constant, colors 
+from shared import constant, colors, pprint
 import random
 
+
 # TODO: manage iface (interface) for the moment I use lo
+class client:
+    def __init__(self, host, port):
+        self.state = 'CLOSED'
+        pprint.state(self.state)
 
-SYNACK = constant.SYN | constant.ACK
+        self.packet = IP()/TCP()
+        self.packet[IP].dst      = host
+        self.packet[TCP].sport   = 2222
+        self.packet[TCP].dport   = port
+        self.packet[TCP].seq     = random.randint(1, 2048) # TODO: Check RFC
 
-def client_connect(host, port):
-    print(colors.OKGREEN + '[*]\tSYN_SENT' + colors.ENDC)
-    packet = IP()/TCP()
+    def connection(self):
+        self.state = 'SYN_SENT'
+        pprint.state(self.state)
+        
+        # Send SYN
+        self.packet[TCP].flags   = 'S'
+        res = sr1(self.packet, iface='lo', timeout=10)
     
-    packet[IP].dst      = host
-    packet[TCP].sport   = 2222
-    packet[TCP].dport   = port
-    packet[TCP].seq     = random.randint(1, 2048) # TODO: Check RFC
-    packet[TCP].flags   = 'S'
-    
-    res = sr1(packet, iface = 'lo', timeout = 10)
+        if res is None:
+            # TODO: handle error
+            pprint.error('Cannot reach host {host} on port {port}'.format(host = self.packet[IP].dst, port = self.packet[TCP].dport))
+        elif res[TCP].flags == constant.SYN | constant.ACK:
+            # TODO: Check ACK value
+            
+            # Send ACK
+            self.packet[TCP].seq    = res[TCP].ack + 1
+            self.packet[TCP].ack    = res[TCP].seq + 1
+            self.packet[TCP].flags  = 'A'
+            send(self.packet, iface='lo' )
 
-    if res is None:
-        # TODO: handle error
-        print('Cannot reach host {host} on port {port}'.format(host = host, port = port))
-    elif res[TCP].flags == SYNACK:
-        # TODO: Manage connexion - Connexion success
-        print(colors.OKGREEN + '[*]\tESTABLISHED' + colors.ENDC)
-        reply = IP()/TCP()
+            # Start communication
+            self.communication()
 
-        reply[TCP].sport    = res[TCP].dport
-        reply[TCP].dport    = res[TCP].sport
-        reply[TCP].seq      = res[TCP].ack + 1
-        reply[TCP].ack      = res[TCP].seq + 1
-        reply[TCP].flags    = 'A'
-        # TODO: Check seq and ack 
-        send(reply, iface = 'lo')
-        # TODO: Manage send information
+        else:
+            # TODO: Deconnection
+            print('Close')
+
+
+    def communication(self):
+        self.state = 'ESTABLISHED'
+        pprint.state(self.state)
         try:
             while True:
                 message = raw_input('Put the message you want to send: ')
-                com = IP()/TCP()/Raw(message)
-                com[TCP].sport    = reply[TCP].sport
-                com[TCP].dport    = reply[TCP].dport
-                com[TCP].seq      = reply[TCP].seq + 1 
-                com[TCP].ack      = reply[TCP].ack  
-                com[TCP].flags    = 'PA'
-                
-                ack = sr1(com, iface='lo', timeout=10)
-                if ack is None:
+                self.packet[TCP].flags    = 'PA'
+
+                ack = sr1(self.packet/Raw(message), iface='lo', timeout=10)
+                if len(ack) <= 0:
                     # TODO: Handle error
-                    print('Did not received ack for data')
-                elif ack[TCP].flags == constant.ACK and ack[TCP].ack == (com[TCP].seq + len(message)):
-                    print(colors.OKBLUE + '...\tMessage received' + colors.ENDC)
+                     pprint.error('Did not received ack for data')
+                elif ack[TCP].flags == constant.ACK and ack[TCP].ack == (self.packet[TCP].seq + len(message)):
+                    pprint.information('Message received')
         except KeyboardInterrupt:
             # TODO: Close connexion
-            print('Close')
-    else:
-        # TODO: Close connexion
-        print('Close')
+             print('Close')
+
+
+
+def client_connect(host, port):
+    c = client(host, port)
+    c.connection()
---
+++ shared/pprint.py
@@ -0,0 +1,14 @@
+import colors
+
+def state(current_state):
+    print(colors.OKGREEN + '[*]\t' + current_state + colors.ENDC)
+
+def information(info):
+    print(colors.OKBLUE + '...\t' + info + colors.ENDC)
+
+def error(error):
+    print(colors.FAIL + '[!]\t' + error + colors.ENDC)
+
+
+
+
